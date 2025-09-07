75728d62037c1c8fffe6fac56e76999766e40c55
firo74 <firo744@gmail.com>
2025-09-07 21:46:19 +0330

    Improve Base model and Add BaseManager and add all_objects

--- core/models.py
+++ core/models.py
@@ -1,19 +1,34 @@
 from django.db import models
+from django.utils import timezone
+
+class BaseManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
 
 class BaseModel(models.Model):
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    objects = BaseManager()
+    all_objects = models.Manager()
+    
 
     class Meta:
         abstract = True
 
     def logical_delete(self):
         self.is_deleted = True
-        self.save()
+        self.deleted_at = timezone.now()
+        self.save(update_fields=['is_deleted', 'deleted_at'])
     
     def restore(self):
         self.is_deleted = False
-        self.save()
-        
+        self.deleted_at = None
+        self.save(update_fields=['is_deleted', 'deleted_at'])
+
+    def delete(self, using = None, keep_parents = False):
+        self.logical_delete()
