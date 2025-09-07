b9b9733d3ac808fa86af58af4114c1d2ee71ed8e
firo74 <firo744@gmail.com>
2025-09-07 13:50:51 +0330

    Create base model in core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    def logical_delete(self):
+        self.is_deleted = True
+        self.save()
+    
+    def restore(self):
+        self.is_deleted = False
+        self.save()
+        
