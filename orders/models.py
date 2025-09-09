0397d18507324b95778cabcc246a0cf77eda545d
Firooz Hosseini <firo744@gmail.com>
2025-09-09 13:23:32 +0330

    Add order model

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from core.models import BaseModel
-from accounts.models import CustomUser
+from accounts.models import CustomUser, Address
 from stores.models import StoreItem
+
 
 class Cart(BaseModel):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='cart_user')
@@ -21,3 +22,14 @@ class CartItem(BaseModel):
     def __str__(self):
         return f'{self.store_item.product.name} x {self.quantity}'
 
+
+class Order(BaseModel):
+    ORDER_STATUS = [(1,'Pending'),(2,'Processing'),(3,'Delivered'),(4,'Cancelled'),(5,'Failed')]
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='order_customer')
+    customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='order_address')
+    status = models.PositiveIntegerField(choices=ORDER_STATUS, default=1)
+    total_price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    total_discount = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f'Order #{self.id} - {self.customer.email}'
