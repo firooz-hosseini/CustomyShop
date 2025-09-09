3a713b6766f612000555af58f0b8855e6e6529c0
Firooz Hosseini <firo744@gmail.com>
2025-09-09 12:08:12 +0330

    Add storeitem model to stores app

--- products/models.py
+++ products/models.py
@@ -5,14 +5,13 @@ from accounts.models import CustomUser
 class Product(BaseModel):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    stock = models.PositiveIntegerField(default=0)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='product_category')
     is_active = models.BooleanField(default=True)
 
 
     def __str__(self):
         return self.name
+
 
 class Category(BaseModel):
     name = models.CharField(max_length=100)
--- stores/models.py
+++ stores/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from core.models import BaseModel
 from accounts.models import CustomUser ,Address
+from products.models import Product
 
 class Store(BaseModel):
     name = models.CharField(max_length=20)
     description = models.TextField(blank=True)
     address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='store_address')
     seller = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='store_seller')
+
+
+class StoreItem(BaseModel):
+    stock = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='item_product')
+    Store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='item_store')
+    
 
 
 
