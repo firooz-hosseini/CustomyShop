d4c5a33edf2efd772f0995aca34f67b7f5f1fdd3
Firooz Hosseini <firo744@gmail.com>
2025-09-09 12:45:51 +0330

    Add cart model to orders app

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.models import BaseModel
+from accounts.models import CustomUser
 
-# Create your models here.
+class Cart(BaseModel):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='cart_user')
+    total_price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    total_discount = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f'Cart of {self.user.email}'
+
+    
