f4fa20833a15e477224be45d47af2858a21df9fb
firo74 <firo744@gmail.com>
2025-09-14 13:34:33 +0330

    Create RequestOtpView

--- CustomyShop/settings.py
+++ CustomyShop/settings.py
@@ -133,3 +133,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from .models import CustomUser
+from .serializers import RequestOtpSerializer
+from random import randint
+from django.core.cache import cache
+from django.core.mail import send_mail
+from rest_framework.response import Response
 
-# Create your views here.
+
+class RequestOtpView(viewsets.GenericViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = RequestOtpSerializer
+
+    def create(self, request):
+        serializer = RequestOtpSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data.get('email')
+            password = serializer.validated_data.get('password')
+            mobile = serializer.validated_data.get('mobile')
+            firs_name = serializer.validated_data.get('first_name')
+            last_name = serializer.validated_data.get("last_name")
+
+
+            otp_code = str(randint(100000,999999))
+            cache.set(f'otp_{otp_code}', {
+                'email': email,
+                'password': password,
+                'mobile': mobile,
+                'firs_name': firs_name,
+                'last_name': last_name,
+
+            }, timeout=300)
+            send_mail(
+                subject='OTP code',
+                message=f'Your OTP code: {otp_code}',
+                from_email='firo744@gmail.com',
+                recipient_list=[email],
+            )
+            return Response({'message': 'OTP is sent'}, status= status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+            
+
+
