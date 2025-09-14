c07709943318dc5851be367bcc5b01e9248ecd40
firo74 <firo744@gmail.com>
2025-09-15 00:57:20 +0330

    Create logout view and url

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,8 @@ from django.core.mail import send_mail
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+
 
 class RequestOtpApiView(viewsets.GenericViewSet):
     queryset = CustomUser.objects.all()
@@ -110,3 +112,22 @@ class LoginApiView(viewsets.GenericViewSet):
                 )
             return Response({'error': 'Invalid email or password'}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class LogoutApiView(viewsets.GenericViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request):
+        try: 
+            refresh_token = request.data.get('refresh')
+            if not refresh_token:
+                return Response({'error': 'Refresh token is required'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({'message': 'Successfully logged out from this device'}, status=status.HTTP_200_OK)
+        
+        except TokenError:
+            return Response({'error': 'Invalid or expired token'}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
