cc7f27a238df12b84996f1c2b0893af2c8e66682
firo74 <firo744@gmail.com>
2025-09-15 13:47:09 +0330

    Add more validations

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,17 +1,40 @@
 from rest_framework import serializers
 from .models import CustomUser
+import re
+
 
 class RequestOtpSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only = True)
+    password = serializers.CharField(write_only = True, min_length = 8)
 
     class Meta:
         model = CustomUser
         fields = ['email', 'password', 'phone', 'first_name', 'last_name']
 
+    def validate_password(self, value):
+        if not re.search(r'[A-Za-z]', value) or not re.search(r'\d', value):
+            raise serializers.ValidationError(
+                'Password must contain at least one letter and one number'
+            )
+        return value
+    
+    def validate_first_name(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError('First name must contain only letters')
+        return value
+    
+    def validate_last_name(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError('Last name must contain only letters')
+        return value
+    
 
 class VerifyOtpSerializer(serializers.Serializer):
     otp_code = serializers.CharField(max_length=6,write_only=True)
 
+    def validate_otp_code(self, value):
+        if not value.isdigit() or len(value) !=6:
+            raise serializers.ValidationError('OTP must be a 6-digit number')
+        return value
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
