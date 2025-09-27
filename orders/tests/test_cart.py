b6337f3569651cde9b304dcd42fa1e0776928d11
firo74 <firo744@gmail.com>
2025-09-27 14:40:51 +0330

    Add test_cart.py for testing cart

--- orders/tests/test_cart.py
+++ orders/tests/test_cart.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from django.contrib.auth import get_user_model
 from products.models import Product, Category
 from stores.models import Store, StoreItem
-from orders.models import Cart, CartItem
+from orders.models import Cart
 
 User = get_user_model()
 
@@ -23,39 +23,68 @@ class CartTests(APITestCase):
         url = reverse("mycart-add-to-cart")
         data = {"store_item_id": self.store_item.id, "quantity": 2}
         response = self.client.post(url, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data["items"][0]["quantity"], 2)
         self.assertEqual(response.data["items"][0]["store_item_id"], self.store_item.id)
 
     def test_update_cart_item_quantity(self):
-        url = reverse("mycart-add-to-cart")
+        add_url = reverse("mycart-add-to-cart")
         data = {"store_item_id": self.store_item.id, "quantity": 2}
-        response = self.client.post(url, data, format="json")
-        cart_item_id = response.data["items"][0]["store_item_id"]
+        response = self.client.post(add_url, data, format="json")
 
+        cart = Cart.objects.get(user=self.user)
+        cart_item_id = cart.cartitem_cart.first().id
 
         url = reverse("mycart-update-quantity")
         data = {"cart_item_id": cart_item_id, "quantity": 3}
         response = self.client.patch(url, data, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["items"][0]["quantity"], 3)
 
 
     def test_remove_item_from_cart(self):
-        url = reverse("mycart-add-to-cart")
-        data = {"store_item_id": self.store_item.id, "quantity": 2}
-        response = self.client.post(url, data, format="json")
-        cart_item_id = response.data["items"][0]["store_item_id"]
-
-
-        url = reverse("mycart-remove-item")
-        response = self.client.delete(url, {"cart_item_id": cart_item_id}, format="json")
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(len(self.client.get(reverse("mycart-detail")).data["items"]), 0)
-
+        item1 = StoreItem.objects.create(store=self.store, product=self.product, price=100, stock=5)
+        item2 = StoreItem.objects.create(store=self.store, product=self.product, price=100, stock=5)
         
-    # def test_add_more_than_stock(self):
-    #     url = reverse("mycart-add-to-cart")
-    #     data = {"store_item_id": self.store_item.id, "quantity": 999}
-    #     response = self.client.post(url, data, format="json")
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        add_url = reverse("mycart-add-to-cart")
+        self.client.post(add_url, {"store_item_id": item1.id, "quantity": 1}, format="json")
+        self.client.post(add_url, {"store_item_id": item2.id, "quantity": 1}, format="json")
+
+        cart = Cart.objects.get(user=self.user)
+        cart_item = cart.cartitem_cart.first()
+
+        remove_url = reverse("mycart-remove-item", kwargs={"pk": cart_item.id})
+        response = self.client.delete(remove_url)
+        self.assertEqual(response.status_code, 200)
+
+        retrieve_url = reverse("mycart-detail", kwargs={"pk": cart_item.id})
+        response = self.client.get(retrieve_url)
+        self.assertEqual(response.status_code, 404)
+
+        list_url = reverse("mycart-list")
+        response = self.client.get(list_url)
+        self.assertEqual(len(response.data["items"]), 1)
+
+    def test_add_more_than_stock(self):
+        url = reverse("mycart-add-to-cart")
+        data = {"store_item_id": self.store_item.id, "quantity": 10}  # stock is only 5
+        response = self.client.post(url, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("message", response.data) 
+        self.assertEqual(response.data["message"], "Only 5 items available in stock.")
+
+    def test_decrease_quantity_to_zero_removes_item(self):
+        add_url = reverse("mycart-add-to-cart")
+        response = self.client.post(add_url, {"store_item_id": self.store_item.id, "quantity": 2}, format="json")
+
+        cart = Cart.objects.get(user=self.user)
+        cart_item_id = cart.cartitem_cart.first().id
+
+        update_url = reverse("mycart-update-quantity")
+        response = self.client.patch(update_url, {"cart_item_id": cart_item_id, "quantity": 0}, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["items"]), 0)
