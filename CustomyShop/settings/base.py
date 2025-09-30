6633c1561febddbb4594f444aed5cdbf45dfcf7f
firo74 <firo744@gmail.com>
2025-09-30 13:52:45 +0330

    Add base_sitee.html

--- CustomyShop/settings/base.py
+++ CustomyShop/settings/base.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'CustomyShop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -138,42 +138,45 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-STORAGES = {
-    "default": {
-        "BACKEND": "storages.backends.s3.S3Storage",
-        "OPTIONS": {
-            "access_key": os.getenv("AWS_ACCESS_KEY_ID"),
-            "secret_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
-            "bucket_name": os.getenv("AWS_STORAGE_BUCKET_NAME"),
-            "endpoint_url": os.getenv("AWS_S3_ENDPOINT_URL"),
-            "region_name": os.getenv("AWS_S3_REGION_NAME"),
-            "addressing_style": "path",   
-            "signature_version": "s3v4",  
-            "default_acl": None,          
-            "file_overwrite": False,        
-            },
-        },
+# STORAGES = {
+#     "default": {
+#         "BACKEND": "storages.backends.s3.S3Storage",
+#         "OPTIONS": {
+#             "access_key": os.getenv("AWS_ACCESS_KEY_ID"),
+#             "secret_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
+#             "bucket_name": os.getenv("AWS_STORAGE_BUCKET_NAME"),
+#             "endpoint_url": os.getenv("AWS_S3_ENDPOINT_URL"),
+#             "region_name": os.getenv("AWS_S3_REGION_NAME"),
+#             "addressing_style": "path",   
+#             "signature_version": "s3v4",  
+#             "default_acl": None,          
+#             "file_overwrite": False,        
+#             },
+#         },
 
-        "staticfiles": {
-        "BACKEND": "storages.backends.s3.S3Storage",
-        "OPTIONS": {
-            "access_key": os.getenv("AWS_ACCESS_KEY_ID"),
-            "secret_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
-            "bucket_name": os.getenv("AWS_STATIC_BUCKET_NAME", os.getenv("AWS_STORAGE_BUCKET_NAME")),
-            "endpoint_url": os.getenv("AWS_S3_ENDPOINT_URL"),
-            "region_name": os.getenv("AWS_S3_REGION_NAME"),
-            "addressing_style": "path",
-            "signature_version": "s3v4",
-            "default_acl": "public-read", 
-        },
-    },
-}
+#         "staticfiles": {
+#         "BACKEND": "storages.backends.s3.S3Storage",
+#         "OPTIONS": {
+#             "access_key": os.getenv("AWS_ACCESS_KEY_ID"),
+#             "secret_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
+#             "bucket_name": os.getenv("AWS_STATIC_BUCKET_NAME", os.getenv("AWS_STORAGE_BUCKET_NAME")),
+#             "endpoint_url": os.getenv("AWS_S3_ENDPOINT_URL"),
+#             "region_name": os.getenv("AWS_S3_REGION_NAME"),
+#             "addressing_style": "path",
+#             "signature_version": "s3v4",
+#             "default_acl": "public-read", 
+#         },
+#     },
+# }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
