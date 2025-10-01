9df1a3daf960e20f9c28f356c24c8cbed042763b
firo74 <firo744@gmail.com>
2025-10-01 14:11:44 +0330

    Add admin inline, filters and search fields for accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,43 @@
 from django.contrib import admin
 from .models import CustomUser, Address
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(CustomUser)
-admin.site.register(Address)
 
+class AddressInline(admin.TabularInline):
+    model = Address
+    extra = 0
+    fields = ('id', 'label', 'address_line_1', 'address_line_2', 'city', 'postal_code', 'country', 'is_default')
+    readonly_fields = ()
+    show_change_link = True
+
+    
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    list_display = ['id', 'email', 'first_name', 'last_name', 'is_staff', 'is_active']
+    list_filter = ['is_staff', 'is_active']
+    search_fields = ['id', 'email', 'first_name', 'last_name']
+    ordering = ['-id']
+    inlines = [AddressInline] 
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'image')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    readonly_fields = ['last_login', 'date_joined']
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'store', 'label', 'address_line_1', 'address_line_2', 'city', 'postal_code', 'country', 'is_default')
+    list_filter = ('city', 'country', 'is_default')
+    search_fields = ('id','user__email', 'address_line_1', 'city', 'postal_code', 'label')
+    ordering = ('id', 'user',)
