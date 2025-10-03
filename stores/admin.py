03f15684ef1390688bc6ecc4e9b907ef43cef126
firo74 <firo744@gmail.com>
2025-10-03 10:33:57 +0330

    Add admin permission to stores admin

--- stores/admin.py
+++ stores/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Store, StoreItem, SellerRequest
 from django.utils import timezone
+
+from accounts.admin_utils import is_seller, is_superadmin, is_support
+
+from .models import SellerRequest, Store, StoreItem
 
 
 class StoreItemInline(admin.TabularInline):
@@ -14,43 +17,118 @@ class StoreItemInline(admin.TabularInline):
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'seller', 'description')
-    list_filter = ('id', 'seller',)
+    list_filter = (
+        'id',
+        'seller',
+    )
     search_fields = ('id', 'name', 'seller__email')
-    ordering = ('-id', 'name',)
+    ordering = (
+        '-id',
+        'name',
+    )
     inlines = [StoreItemInline]
-    
 
-@admin.action(description="Enable selected store items")
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if is_superadmin(request.user):
+            return qs
+        if is_seller(request.user):
+            return qs.filter(owner=request.user)
+        return qs.none()  # SupportStaff and Customer cannot access Stores
+
+    def has_change_permission(self, request, obj=None):
+        if is_superadmin(request.user):
+            return True
+        if is_seller(request.user) and obj:
+            return obj.owner == request.user
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return self.has_change_permission(request, obj)
+
+    def has_add_permission(self, request):
+        return is_superadmin(request.user) or is_seller(request.user)
+
+
+@admin.action(description='Enable selected store items')
 def enable_store_items(modeladmin, request, queryset):
     queryset.update(is_active=True)
 
 
-@admin.action(description="Disable selected store items")
+@admin.action(description='Disable selected store items')
 def disable_store_items(modeladmin, request, queryset):
     queryset.update(is_active=False)
 
+
 @admin.register(StoreItem)
 class StoreItemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'product', 'store', 'stock', 'price', 'discount_price', 'is_active')
+    list_display = (
+        'id',
+        'product',
+        'store',
+        'stock',
+        'price',
+        'discount_price',
+        'is_active',
+    )
     list_filter = ('id', 'store', 'is_active')
     search_fields = ('id', 'product__name', 'store__name')
-    ordering = ('id', 'store', 'product',)
+    ordering = (
+        'id',
+        'store',
+        'product',
+    )
     actions = [enable_store_items, disable_store_items]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if is_superadmin(request.user):
+            return qs
+        if is_seller(request.user):
+            return qs.filter(store__owner=request.user)
+        if is_support(request.user):
+            return qs.all()  # SupportStaff can view all store items
+        return qs.none()
+
+    def has_change_permission(self, request, obj=None):
+        if is_superadmin(request.user):
+            return True
+        if is_seller(request.user) and obj:
+            return obj.store.owner == request.user
+        if is_support(request.user):
+            return False
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return self.has_change_permission(request, obj)
+
+    def has_add_permission(self, request):
+        return is_superadmin(request.user) or is_seller(request.user)
+
 
 @admin.register(SellerRequest)
 class SellerRequestAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'status', 'created_at', 'reviewed_at')
-    list_filter = ('id', 'status',)
-    search_fields = ('id', 'user__email',)
-    ordering = ('id', '-reviewed_at',)
+    list_filter = (
+        'id',
+        'status',
+    )
+    search_fields = (
+        'id',
+        'user__email',
+    )
+    ordering = (
+        'id',
+        '-reviewed_at',
+    )
 
     actions = ['approve_request', 'reject_request']
 
     def approve_request(self, request, queryset):
         for req in queryset:
             if req.status != SellerRequest.PENDING:
-                continue 
-            
+                continue
+
             req.status = SellerRequest.APPROVED
             req.reviewed_at = timezone.now()
             req.save()
@@ -60,7 +138,7 @@ class SellerRequestAdmin(admin.ModelAdmin):
             user.is_seller = True
             user.save()
 
-    approve_request.short_description = "Approve selected requests"
+    approve_request.short_description = 'Approve selected requests'
 
     def reject_request(self, request, queryset):
         for req in queryset:
@@ -70,6 +148,4 @@ class SellerRequestAdmin(admin.ModelAdmin):
             req.reviewed_at = timezone.now()
             req.save()
 
-    reject_request.short_description = "Reject selected requests"
-
-
+    reject_request.short_description = 'Reject selected requests'
